c98c88a9b2ff6865c9fd8a31cc3aad8a9b1652c9
Grégori Sabel <seriosabel57@gmail.com>
2022-06-08 17:29:15 -0300

    slide stops at the end

--- src/components/Genres/Genre.tsx
+++ src/components/Genres/Genre.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 import { Movie } from "../Movie";
-import { Arrows, GenreComponent, MovieList } from "./styles";
+import { Arrow, GenreComponent, MovieList } from "./styles";
 
 interface MovieType{
   id: string;
@@ -23,28 +23,36 @@ export default function Genre({ slug, title, items}:GenreData) {
   const [slideAmount, setSlideAmount] = useState(0)
 
   function slideLeft(){
-    const newSlideAmount = slideAmount + 500 
+    const newSlideAmount = slideAmount + window.innerWidth 
     setSlideAmount((newSlideAmount > 0 ? 0 : newSlideAmount))
   }
 
   function slideRight(){
-    const newSlideAmount = slideAmount - 500 
-    setSlideAmount(newSlideAmount)
+    const newSlideAmount = slideAmount - window.innerWidth 
+    const itemsWidth = -1 * ((items.results.length * (285 + 10) + 60 + 20) - window.innerWidth)
+    console.log(itemsWidth)
+    if( newSlideAmount < (itemsWidth) ){
+      setSlideAmount(itemsWidth)
+    }else{
+      setSlideAmount(newSlideAmount)
+    }
   }
 
 
   return(
     <>
       <GenreComponent >
-        <h1 >{title}</h1> 
-        <Arrows slideAmount={slideAmount}>
+        <h1 >{title} | {slideAmount}</h1> 
+        <Arrow slideAmount={slideAmount} side='left'>
           <button type="button" onClick={() => slideLeft()}>
             <FiChevronLeft size='50px' />
           </button>
+        </Arrow>
+        <Arrow slideAmount={slideAmount} side='right'> 
           <button type="button" onClick={slideRight}>
             <FiChevronRight size='50px' />
           </button>
-        </Arrows>
+        </Arrow>
         <MovieList slideAmount={slideAmount}>
           {items.results.map(movie => (
             <Movie key={movie.id} title={movie.original_title} poster={'https://image.tmdb.org/t/p/w780/'+movie.poster_path}/>
--- src/components/Genres/styles.ts
+++ src/components/Genres/styles.ts
@@ -4,6 +4,12 @@ interface slideAmountProps{
   slideAmount: number;
 }
 
+
+interface ArrowProps{
+  slideAmount: number;
+  side: string;
+}
+
 export const Component = styled.div`
 
   overflow: hidden;
@@ -36,7 +42,7 @@ export const MovieList = styled.div<slideAmountProps>`
   /* position: absolute;
   z-index: 0; */
   transform: translateX( ${(props) => (props.slideAmount)}px);
-  transition: 0.5s ease-in-out;
+  transition: 0.8s cubic-bezier(.2,.01,.41,.99);
 
   
   /* left: ${(props) => (props.slideAmount)}px;
@@ -48,24 +54,48 @@ export const MovieList = styled.div<slideAmountProps>`
 
 `
 
-export const Arrows = styled.div<slideAmountProps>`
-  width: 100% ;
-  display: flex;
-  justify-content: space-between;
+export const Arrow = styled.div<ArrowProps>`
+  /* width: 100%; */
+  /* display: flex; */
+  /* justify-content: space-between; */
 
   position: absolute;
-  top: 150px;
+  /* left: -60px; */
+
+  ${props => (props.side === 'left' ? 
+    {left: -60}
+   : 
+    {right: 0}
+  )}
+
+  top: 97px;
+  /* top: 57px; */
   z-index: 1000;
 
   button{
-    background-color: transparent;
+    height: 160px;
+    background-color: rgba(0, 0, 0, 0.3);
+    
+    border: none;
+    border-radius: 5px;
     color: white;
-    border: none
+    
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    transition: opacity 0.2s ease-out;
+
+    &:hover{
+      background-color: rgba(0, 0, 0, 0.7);
+    }
   }
 
-  button:first-child{
-    opacity: ${(props) => (props.slideAmount < 0 ? 100 : 0)}% ;
-    transition: opacity 0.5s;
-  }
+
+  opacity: 100%;
+
+  opacity: ${(props) => ((props.side === 'right') || props.slideAmount < 0 ? 100 : 0)}% ;
+  transition: opacity 0.5s;
+
 
 `
